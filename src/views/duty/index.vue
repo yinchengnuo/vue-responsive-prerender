3e71b8ec2b82f0817e6a700eb080ca5f49e6cbfc
yinchengnuo <yinnuo96@163.com>
2020-12-08 11:11:47 +0800

    updata

--- src/views/duty/index.vue
+++ src/views/duty/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="duty">
     <div class="layout">
-      <img v-if="$route.path === '/duty/love'" src="@/assets/duty/1.png" class="banner">
-      <img v-if="$route.path === '/duty/talk'" src="@/assets/duty/1.png" class="banner">
-      <img v-if="$route.path === '/duty/tour'" src="@/assets/duty/1.png" class="banner">
+      <img v-if="$route.path === '/duty/love'" src="@/assets/duty/1.jpg" class="banner">
+      <img v-if="$route.path === '/duty/talk'" src="@/assets/duty/2.jpg" class="banner">
+      <img v-if="$route.path === '/duty/tour'" src="@/assets/duty/3.jpg" class="banner">
       <div class="navbar-wrapper" :class="{ fixed: this.$store.state.top >= 600 }">
         <div class="navbar">
           <router-link class="router-link" to="/duty/love">《爱的守望》公益行</router-link>
